75bd4461663b3c090e4d021a7cb037b7f429623b
Rashwan <ahmed.t.rashwan@gmail.com>
2016-06-22 18:04:10 +0200

    Initial commit.

---
+++ app/src/main/java/com/rashwan/reactive_popular_movies/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rashwan.reactive_popular_movies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
