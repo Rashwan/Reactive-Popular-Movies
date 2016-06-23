ed826674543475420f1682121615afecc401d4ee
Rashwan <ahmed.t.rashwan@gmail.com>
2016-06-23 18:46:05 +0200

    Add Movie model.

--- app/src/main/java/com/rashwan/reactive_popular_movies/model/Movie.java
+++ app/src/main/java/com/rashwan/reactive_popular_movies/model/Movie.java
@@ -1,8 +1,97 @@
 package com.rashwan.reactive_popular_movies.model;
 
+import com.squareup.moshi.Json;
+
+import java.util.List;
+
 /**
  * Created by rashwan on 6/23/16.
  */
 
 public class Movie {
+    public int id;
+    public String title;
+    public String overview;
+    @Json(name = "release_date") public String releaseDate;
+    @Json(name = "poster_path") public String posterPath;
+    @Json(name = "vote_average") public String voteAverage;
+    @Json(name = "backdrop_path") public String backdropPath;
+    public List<Trailer> trailers = null;
+    public List<Review> reviews = null;
+
+    //Empty constructor for Moshi
+    public Movie() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public List<Trailer> getTrailers() {
+        return trailers;
+    }
+
+    public void setTrailers(List<Trailer> trailers) {
+        this.trailers = trailers;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(String voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
